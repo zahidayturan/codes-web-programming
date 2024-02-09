a4aaa16145846f429322e7854baf13bf9061efb8
zahidayturan <21060607@stu.omu.edu.tr>
2024-02-09 22:48:38 +0300

    edit main page,size
    edit

--- html-practices/fezaitech-com/script.js
+++ html-practices/fezaitech-com/script.js
@@ -1,8 +1,8 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
 
 // Dark / light mode
@@ -13,41 +13,103 @@ const themeIcons = document.querySelectorAll(".icon");
 const currentTheme = localStorage.getItem("theme");
 
 if (currentTheme === "dark") {
-    setDarkMode();
+  setDarkMode();
 }
 
 btn.addEventListener("click", function () {
-    setTheme();
+  btn.classList.toggle("active");
+  setTheme();
 });
 
 btn2.addEventListener("click", function () {
-    setTheme();
+  btn2.classList.toggle("active");
+  setTheme();
 });
 
 function setTheme() {
-    let currentTheme = document.body.getAttribute("theme");
+  let currentTheme = document.body.getAttribute("theme");
 
-    if (currentTheme === "dark") {
-        setLightMode();
-    } else {
-        setDarkMode();
-    }
+  if (currentTheme === "dark") {
+    setLightMode();
+  } else {
+    setDarkMode();
+  }
 }
 
 function setDarkMode() {
-    document.body.setAttribute("theme", "dark");
-    localStorage.setItem("theme", "dark");
+  document.body.setAttribute("theme", "dark");
+  localStorage.setItem("theme", "dark");
 
-    themeIcons.forEach((icon) => {
-        icon.src = icon.getAttribute("src-dark");
-    });
+  themeIcons.forEach((icon) => {
+    icon.src = icon.getAttribute("src-dark");
+  });
+
+  const profile = document.getElementById("profile");
+  profile.style.backgroundImage = 'url("./assets/grid-dark.png")';
+
+  const experience = document.getElementsByClassName("experience-container");
+  experience[0].style.backgroundImage = 'url("./assets/we-dark.png")';
+
+  const works = document.getElementsByClassName("works-container");
+  works[0].style.backgroundImage = 'url("./assets/digits-dark.png")';
 }
 
 function setLightMode() {
-    document.body.removeAttribute("theme");
-    localStorage.setItem("theme", "light");
+  document.body.removeAttribute("theme");
+  localStorage.setItem("theme", "light");
 
-    themeIcons.forEach((icon) => {
-        icon.src = icon.getAttribute("src-light");
+  themeIcons.forEach((icon) => {
+    icon.src = icon.getAttribute("src-light");
+  });
+
+  const profile = document.getElementById("profile");
+  profile.style.backgroundImage = 'url("./assets/grid-light.png")';
+
+  const experience = document.getElementsByClassName("experience-container");
+  experience[0].style.backgroundImage = 'url("./assets/we-light.png")';
+
+  const works = document.getElementsByClassName("works-container");
+  works[0].style.backgroundImage = 'url("./assets/digits-light.png")';
+}
+
+const languages = {
+  tr: {
+    language: "EN",
+    idea__1: "Fikirlerinizi",
+    idea__2: "Gerçeğe",
+    idea__3: "Dönüştürüyoruz",
+
+  },
+  en: {
+    language: "TR",
+    idea__1: "We Bring",
+    idea__2: "Your Ideas",
+    idea__3: "To Life",
+
+  },
+};
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const currentLanguage = localStorage.getItem("language") || "tr";
+  setLanguage(currentLanguage);
+});
+
+
+function toggleLanguage() {
+  const currentLanguage = localStorage.getItem("language") || "tr";
+  const newLanguage = currentLanguage === "tr" ? "en" : "tr";
+  setLanguage(newLanguage);
+}
+
+
+function setLanguage(language) {
+  localStorage.setItem("language", language);
+
+  Object.keys(languages[language]).forEach(function (key) {
+    const elements = document.querySelectorAll(`[data-translate="${key}"]`);
+    elements.forEach(function (element) {
+      element.textContent = languages[language][key];
     });
+  });
 }
